4323ec1577db43afb0125a0b0bfd7862e0549d16
Mariz Melo <mm@xchema.com>
2013-03-13 17:46:50 -0700

    todo file, ignore change, DORM.js changes - rendering HTML (basic)

--- DORM.js
+++ DORM.js
@@ -1,16 +1,85 @@
-/*  
-  DORM (Document Object Render Model)
-  author: Mariz Melo
-  github: marizmelo/DORM
-*/
-var cheerio = require('cheerio')
-  , DORM = function() {}  // name-space
-  , d = DORM.prototype;
+var DORM = function(){};
+var dorm = DORM.prototype;
 
-d.controller = {
-  _render : function(obj) {
-    if('head' in obj){
-
-    }
-  }
+// receive an array with classes of the element and generate html syntax
+dorm.class = function(classes) {
+  return " class="+escape(classes.toString());
 }
+
+dorm.render = function(dom) {
+    // console.log("Number of elements: "+Object.keys(dom).length);
+      for (var key in dom) {
+
+        console.log("<"+key+">");
+        if( typeof(dom[key]) == "object") {
+        console.log( "\t" + Object.keys(dom[key]) );
+      }else{
+        console.log(dom[key]);
+      }
+
+        
+        if(dom[key]["children"] && dom[key]["children"].length > 0) {
+
+          for(var i=0; i<= dom[key]["children"].length; ++i) {
+            dorm.render(dom[key]["children"][i]);
+          }          
+      }
+      
+      console.log("</"+key+">");
+        
+    }
+};
+
+dorm.render({
+  "html": {
+    "events" : {
+      click : function(){}
+    },
+    "attr" : {
+      "class": [
+      "responsive"
+      ]
+    },
+    "children": [
+      {
+        "head": {
+          "children": [
+            {
+              "meta": {
+              },
+              "link": {
+              },
+              "title": "hello world"
+            }
+          ]
+        }
+      },
+      {
+        "body": {
+          "children": [
+            {
+              "div": {
+                attr : {
+                  "id": "mydiv"
+                }
+              }
+            },
+            {
+              "div": {
+                attr : {
+                  "id": "mydiv2"
+                },
+                children:[
+                  { "span" :{
+                      "text" : "hello you"
+                    } 
+                  }
+                ]
+              }
+            }
+          ]
+        }
+      }
+    ]
+  }
+});
