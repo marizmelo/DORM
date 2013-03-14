63ded50459a146843a3f09b056763975e298c0b1
Mariz Melo <mm@xchema.com>
2013-03-13 21:47:41 -0700

    dealing with self-closing tags (<meta>,<br>,etc)

--- DORM.js
+++ DORM.js
@@ -1,6 +1,10 @@
 var DORM = function(){};
 var dorm = DORM.prototype;
 
+// data
+dorm.model = {
+  selfclose : ["meta", "br", "link"]
+}
 
 dorm.attr = function (attr) {
   // return HTML attributes to object
@@ -29,32 +33,36 @@ dorm.class = function(classes) {
 
 dorm.render = function(dom) {
     // console.log("Number of elements: "+Object.keys(dom).length);
-      for (var key in dom) {
+  for (var key in dom) {
 
-        var opentag = "<"+key;
-        if(dom[key]["attr"]){
-          opentag += dorm.attr(dom[key]["attr"]) + ">";
-        }else{
-          opentag += ">";
-        }
-        console.log(opentag);
-        if( typeof(dom[key]) == "object") {
-        console.log( "\t" + Object.keys(dom[key]) );
-      }else{
-        console.log(dom[key]);
-      }
+    var opentag = "<"+key;
+    
+    if(dom[key]["attr"]){
+      opentag += dorm.attr(dom[key]["attr"]) + ">";
+    }else{
+      opentag += ">";
+    }//if_else
+    
+    console.log(opentag);
+    
+    if( typeof(dom[key]) == "object") {
+      console.log( "\t" + Object.keys(dom[key]) );
+    }else{
+      console.log(dom[key]);
+    }//if_else
+    
+    if(dom[key]["children"] && dom[key]["children"].length > 0) {
 
-        
-        if(dom[key]["children"] && dom[key]["children"].length > 0) {
-
-          for(var i=0; i<= dom[key]["children"].length; ++i) {
-            dorm.render(dom[key]["children"][i]);
-          }          
-      }
-      
+      for(var i=0; i<= dom[key]["children"].length; ++i) {
+        dorm.render(dom[key]["children"][i]);
+      }//for
+    }//if
+  
+    // check for self-closing elements
+    if(dorm.model.selfclose.indexOf(key) == -1){
       console.log("</"+key+">");
-        
-    }
+    }//if
+  }//for
 };
 
 dorm.render({
@@ -86,19 +94,19 @@ dorm.render({
           "children": [
             {
               "div": {
-                attr : {
+                "attr" : {
                   "id": "mydiv"
                 },
-                text : "Hi there"
+                "text" : "Hi there"
               }
             },
             {
               "div": {
-                attr : {
+                "attr" : {
                   "id": "mydiv2",
                   "name": "mydiv2name"
                 },
-                children:[
+                "children":[
                   { "span" :  "hello you" }
                 ]
               }
--- README.md
+++ README.md
@@ -2,3 +2,14 @@ DORM
 ====
 
 DORM (Document Object Render Model). Render HTML elements and attach properties to them.
+
+
+TODO
+----
+     1	+ bind JavaScript events
+     2	+ Pretty identation for output
+     3	+ Insert TEXT as content of TAG
+     4	+ Insert ATTR on tags
+     5	+ find solution for <span> in the middle of TEXT
+     6	- Insert inline stylesheets
+     7	- deal with self-closed elements like <br>
